e41dd6375564dc02389f4a2510148c6ea861eae1
ll <ll@example.com>
2018-09-01 16:05:05 +0800

    add class Base...

--- zaixian/apps/base/models.py
+++ zaixian/apps/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import django.utils.timezone as timezone
 
 # Create your models here.
+class Base(models.Model):
+
+    def Meta(object):
+        abstract = True
+
+    createUser = models.CharField(max_length=32, null=True)
+    updateUser = models.CharField(max_length=32, null=True)
+    createTime = models.DateTimeField(auto_now=True)
+    updateTime = models.DateTimeField(null=True, default=timezone)
+    status = models.CharField(max_length=16, null=True, default=None)
+    isDelete = models.BooleanField(null=True, default=False)
--- zaixian/apps/jobInfo/models.py
+++ zaixian/apps/jobInfo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from apps.base.models import Base
 
 # Create your models here.
+class JobInfo(Base):
+    pass

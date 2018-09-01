1b4a5a8ee944fce80b0b9f006b28ad347f3f7d93
tt <tt@example.com>
2018-09-01 16:58:56 +0800

    tttT

--- zaixian/apps/jobRequirement/models.py
+++ zaixian/apps/jobRequirement/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from apps.base.models import Base
+from apps.jobInfo.models import JobInfo
 
 # Create your models here.
 class JobRequirement(Base):
-    education=models.CharField(max_length=20, null=True, default=None)
-    experience=models.CharField(max_length=256, null=True, default=None)
-    passtime=models.CharField(max_length=32, null=True, default=None)
-    certificate=models.CharField(max_length=128, null=True, default=None)
-    major=models.CharField(max_length=64, null=True, default=None)
-    skill=models.CharField(max_length=128, null=True, default=None)
-    #jobInfos=models.ForeignKey(JobInfo)
+    education = models.CharField(max_length=20, null=True, default=None)
+    experience = models.CharField(max_length=256, null=True, default=None)
+    passTime = models.CharField(max_length=32, null=True, default=None)
+    certificate = models.CharField(max_length=128, null=True, default=None)
+    major = models.CharField(max_length=64, null=True, default=None)
+    skill = models.CharField(max_length=128, null=True, default=None)
+    jobInfo = models.ForeignKey(JobInfo, on_delete=models.CASCADE)
 

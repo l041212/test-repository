099ec6faa7910929ec80862c5bacce81b41fdb42
yangyun <yangyun@example.com>
2018-09-03 10:49:57 +0800

    ttt

--- zaixian/apps/base/models.py
+++ zaixian/apps/base/models.py
@@ -10,10 +10,6 @@ class Base(models.Model):
     createUser = models.CharField(max_length=32, null=True, default=None)
     updateUser = models.CharField(max_length=32, null=True, default=None)
     createTime = models.DateTimeField(auto_now=True)
-<<<<<<< Updated upstream
     updateTime = models.DateTimeField(null=True, default=timezone.now)
-=======
-    updateTime = models.DateTimeField(null=True, default=timezone.now())
->>>>>>> Stashed changes
     status = models.CharField(max_length=16, null=True, default=None)
     isDelete = models.BooleanField(null=True, defaultl=False)

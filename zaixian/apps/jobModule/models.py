7b11c8a73450335b1fecca67d4133726ea4d08c4
ll <ll@example.com>
2018-09-03 10:53:54 +0800

    all modules were updated...

--- zaixian/apps/jobModule/models.py
+++ zaixian/apps/jobModule/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class JobModule(models.Model):
+
+        class Meta(object):
+                db_table = 'apps_JobModule'
+
         #模板名称
-        name = models.CharField(max_length=32,null=True,default=False)
-        #job表的外键
-        job_id = models.CharField(max_length=20,null=True)
-        #文本
-        text = models.CharField(max_length=1024,null=True)
-        #附件
-        attachment = models.CharField(null=True,default=False,max_length=128)
+        name = models.CharField(max_length=32, null=True, default=False)
+        #模板文本
+        text = models.CharField(max_length=1024, null=True, default=False)
+        #模板附件
+        attachment = models.FileField(upload_to='media/attachment', null=True, default=None)
+        #职业信息ID
+        jobInfo_id = models.CharField(max_length=20, null=True, default=False)
--- zaixian/apps/login/models.py
+++ zaixian/apps/login/models.py
@@ -3,11 +3,19 @@ from apps.base.models import Base
 
 # Create your models here.
 class User(Base):
+
+    class Meta(object):
+        db_table = 'apps_User'
+
     # id = models.AutoField(auto_created=True, primary_key=True, serialize=, verbose_name='ID'),
-    name= models.CharField(max_length=32, null=True),
-    code=models.CharField(max_length=32,null=True),
-    role=models.CharField(max_length=16,null=True),
-    email=models.CharField(max_length=64,null=True)
+    #用户名称
+    name = models.CharField(max_length=32, null=True, default=None)
+    #帐号名称
+    code = models.CharField(max_length=32, null=True, default=None)
+    #用户角色
+    role = models.CharField(max_length=16,null=True, default=None)
+    #用户邮箱
+    email = models.CharField(max_length=64, null=True, default=None)
 #   id int(20) primary key,
 #   name varchar(32) default null comment '名称',
 #   code varchar(32) default null comment '帐号名称',

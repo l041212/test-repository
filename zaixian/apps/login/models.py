97b84cf56a4443692268b25cf6251b6abe8a25c4
yangyun <yangyun@example.com>
2018-09-05 16:02:32 +0800

    ttt

--- zaixian/apps/login/models.py
+++ zaixian/apps/login/models.py
@@ -5,11 +5,11 @@ from apps.base.models import Base
 class UserA(Base):
     # id = models.AutoField(auto_created=True, primary_key=True, serialize=, verbose_name='ID'),
     #用户名称
-    # name = models.CharField(max_length=32, null=True, default=None)
+    name = models.CharField(max_length=32, null=True, default=None)
     # #帐号名称
-    # code = models.CharField(max_length=32, null=True, default=None)
+    code = models.CharField(max_length=32, null=True, default=None)
     # #用户角色
-    # role = models.CharField(max_length=16,null=True, default=None)
+    role = models.CharField(max_length=16,null=True, default=None)
     #用户邮箱
     email = models.CharField(max_length=64, null=True, default=None)
     #用户密码
